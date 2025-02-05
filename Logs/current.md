6a05942ba9907ebd71335c6346bc6c30d4462e50
Ilana Sandler <9151o66926@gmail.com>
2025-02-05 19:50:33 +0200

    hist(), boxplot()

--- Logs/current.md
+++ Logs/current.md
@@ -7,3 +7,7 @@
 ## 2025-02-04
 - I've finished my first project on jupyter notebook. It doesnt seem perfect but now I see how exactly it works. I'm on my way to the next project.
 - Starting "Exploratory data analysis" (EDA) chapter in my course
+- hist(bins=, range=())
+- quartus Q1 Q2 Q3
+- boxplot() in python
+- import matplotlib.pyplot
--- Python/eda.py
+++ Python/eda.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt 
 
 data = pd.read_csv('/datasets/visits.csv', sep='\t')
 stat = data.pivot_table(index='name', values='time_spent')
@@ -9,3 +10,10 @@ data['date_time_days'] = pd.to_datetime(
     )
 total_days = data['date_time_days'].max().day - data['date_time_days'].min().day
 station_visits_per_day = total_visits / total_stations / total_days
+
+data['time_spent'].hist(bins=100, range=(0, 1500))
+                        
+plt.ylim(-100, 1000)
+data.boxplot()
+print(data.describe())
+data.hist('age', range=(18, 30))
